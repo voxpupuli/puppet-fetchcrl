de1dd695b1c2953dd08565b86b3e2d1e346e2fa3
Steve Traylen <steve.traylen@cern.ch>
2016-04-26 14:28:42 +0200

    Fix for strict variables

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 describe 'fetchcrl', :type => 'class' do
 
   context 'with defaults for all parameters' do
+    let(:facts) {{osfamily: 'RedHat',
+                  operatingsystemrelease: '6'}}
     it { should contain_class('fetchcrl') }
     it { should contain_class('fetchcrl::install') }
     it { should contain_class('fetchcrl::config') }
@@ -10,7 +12,9 @@ describe 'fetchcrl', :type => 'class' do
     it { should contain_file('/etc/fetch-crl.conf').without_content(/cache_control_request/) }
   end
   context 'with all parameters set' do
-    let(:params) {{:cache_control_request => '1234'}}
+    let(:facts) {{osfamily: 'RedHat',
+                  operatingsystemrelease: '6'}}
+    let(:params) {{cache_control_request: '1234'}}
     it { should contain_file('/etc/fetch-crl.conf').with_content(/^cache_control_request = 1234$/)  }
   end 
 end
