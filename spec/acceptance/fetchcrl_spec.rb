222f1e0d7fb17292d828c0a0efd368af04505bd4
Steve Traylen <steve.traylen@cern.ch>
2016-08-22 11:05:53 +0200

    Rubocop fixes

--- spec/acceptance/fetchcrl_spec.rb
+++ spec/acceptance/fetchcrl_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper_acceptance'
 
 describe 'fetchcrl' do
-  it 'should configure and work with no errors' do
+  it 'configures and work with no errors' do
     pp = <<-EOS
       class{'fetchcrl':
       }
     EOS
-    apply_manifest(pp,:catch_failures => true)
-    apply_manifest(pp,:catch_changes  => true)
-    if fact('osfamily') == 'RedHat' and fact('operatingsystemmajrelease') == '5'  then
-      cmd = 'fetch-crl3'
-    else
-      cmd = 'fetch-crl'
-    end
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+    cmd = if fact('osfamily') == 'RedHat' && fact('operatingsystemmajrelease') == '5'
+            'fetch-crl3'
+          else
+            'fetch-crl'
+          end
     # We accept 1 as a return code also since there will be some failing CAs
     # out there in the wild.
-    shell(cmd,  :acceptable_exit_codes => [0,1])
-    shell('ls /etc/grid-security/certificates/*.r0', :acceptable_exit_codes => 0)
+    shell(cmd, acceptable_exit_codes: [0, 1])
+    shell('ls /etc/grid-security/certificates/*.r0', acceptable_exit_codes: 0)
   end
 end
-
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,20 +1,23 @@
 require 'spec_helper'
-describe 'fetchcrl', :type => 'class' do
-
+describe 'fetchcrl', type: 'class' do
   context 'with defaults for all parameters' do
-    let(:facts) {{osfamily: 'RedHat',
-                  operatingsystemrelease: '6'}}
+    let(:facts) do
+      { osfamily: 'RedHat',
+        operatingsystemrelease: '6' }
+    end
     it { should contain_class('fetchcrl') }
     it { should contain_class('fetchcrl::install') }
     it { should contain_class('fetchcrl::config') }
     it { should contain_class('fetchcrl::service') }
     it { should contain_package('fetch-crl') }
-    it { should contain_file('/etc/fetch-crl.conf').without_content(/cache_control_request/) }
+    it { should contain_file('/etc/fetch-crl.conf').without_content(%r{cache_control_request}) }
   end
   context 'with all parameters set' do
-    let(:facts) {{osfamily: 'RedHat',
-                  operatingsystemrelease: '6'}}
-    let(:params) {{cache_control_request: '1234'}}
-    it { should contain_file('/etc/fetch-crl.conf').with_content(/^cache_control_request = 1234$/)  }
-  end 
+    let(:facts) do
+      { osfamily: 'RedHat',
+        operatingsystemrelease: '6' }
+    end
+    let(:params) { { cache_control_request: '1234' } }
+    it { should contain_file('/etc/fetch-crl.conf').with_content(%r{^cache_control_request = 1234$}) }
+  end
 end
