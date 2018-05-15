91ae94f37dbb14a6cc4cf894ea5373380be63dea
Steve Traylen <steve.traylen@cern.ch>
2018-05-15 13:52:25 +0200

    Remove .el5 support, enable acceptance tests.

--- spec/acceptance/fetchcrl_spec.rb
+++ spec/acceptance/fetchcrl_spec.rb
@@ -8,14 +8,9 @@ describe 'fetchcrl' do
     EOS
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
-    cmd = if fact('osfamily') == 'RedHat' && fact('operatingsystemmajrelease') == '5'
-            'fetch-crl3'
-          else
-            'fetch-crl'
-          end
     # We accept 1 as a return code also since there will be some failing CAs
     # out there in the wild.
-    shell(cmd, acceptable_exit_codes: [0, 1])
+    shell('fetch-crl', acceptable_exit_codes: [0, 1])
     shell('ls /etc/grid-security/certificates/*.r0', acceptable_exit_codes: 0)
   end
 end
