50c5e4b94ccf04af641734b9c97ad6ad10e90584
Steve Traylen <steve.traylen@cern.ch>
2018-11-29 15:33:25 +0100

    Covert unit tests to rspec-puppet-facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,46 +1,41 @@
 require 'spec_helper'
 describe 'fetchcrl', type: 'class' do
-  context 'with defaults for all parameters' do
-    let(:facts) do
-      { osfamily: 'RedHat',
-        operatingsystemmajrelease: '6' }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
 
-    it { is_expected.to contain_class('fetchcrl') }
-    it { is_expected.to contain_class('fetchcrl::install') }
-    it { is_expected.to contain_class('fetchcrl::config') }
-    it { is_expected.to contain_class('fetchcrl::service') }
-    it { is_expected.to contain_package('fetch-crl') }
-    it { is_expected.to contain_file('/etc/fetch-crl.conf').without_content(%r{cache_control_request}) }
-    it { is_expected.to contain_file('/etc/fetch-crl.conf').without_content(%r{noerrors}) }
-  end
-  context 'with all parameters set' do
-    let(:facts) do
-      { osfamily: 'RedHat',
-        operatingsystemmajrelease: '6' }
-    end
-    let(:params) do
-      {
-        cache_control_request: 1234,
-        capkgs: %w[abc def]
-      }
-    end
+      context 'with defaults for all parameters' do
+        it { is_expected.to contain_class('fetchcrl') }
+        it { is_expected.to contain_class('fetchcrl::install') }
+        it { is_expected.to contain_class('fetchcrl::config') }
+        it { is_expected.to contain_class('fetchcrl::service') }
+        it { is_expected.to contain_package('fetch-crl') }
+        it { is_expected.to contain_file('/etc/fetch-crl.conf').without_content(%r{cache_control_request}) }
+        it { is_expected.to contain_file('/etc/fetch-crl.conf').without_content(%r{noerrors}) }
+      end
+      context 'with all parameters set' do
+        let(:params) do
+          {
+            cache_control_request: 1234,
+            capkgs: %w[abc def]
+          }
+        end
 
-    it { is_expected.to contain_file('/etc/fetch-crl.conf').with_content(%r{^cache_control_request = 1234$}) }
-    it { is_expected.to contain_package('abc').with_ensure('present') }
-    it { is_expected.to contain_package('def').with_ensure('present') }
-  end
-  context 'with noerrors parameters set' do
-    let(:facts) do
-      { osfamily: 'RedHat',
-        operatingsystemmajrelease: '6' }
-    end
-    let(:params) do
-      {
-        noerrors: true
-      }
-    end
+        it { is_expected.to contain_file('/etc/fetch-crl.conf').with_content(%r{^cache_control_request = 1234$}) }
+        it { is_expected.to contain_package('abc').with_ensure('present') }
+        it { is_expected.to contain_package('def').with_ensure('present') }
+      end
+      context 'with noerrors parameters set' do
+        let(:params) do
+          {
+            noerrors: true
+          }
+        end
 
-    it { is_expected.to contain_file('/etc/fetch-crl.conf').with_content(%r{^noerrors$}) }
+        it { is_expected.to contain_file('/etc/fetch-crl.conf').with_content(%r{^noerrors$}) }
+      end
+    end
   end
 end
