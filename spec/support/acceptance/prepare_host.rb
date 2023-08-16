537905fbb9a26420d3cd94743e0ef8b53dd818ec
Steve Traylen <steve.traylen@cern.ch>
2023-08-16 10:09:30 +0200

    epel needed for all the family

--- spec/support/acceptance/prepare_host.rb
+++ spec/support/acceptance/prepare_host.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 def prepare_host
-  shell('yum install -y epel-release') if fact('os.name') == 'CentOS'
+  shell('yum install -y epel-release') if fact('os.family') == 'RedHat'
 end
